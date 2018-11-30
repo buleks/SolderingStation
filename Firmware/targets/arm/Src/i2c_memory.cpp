b5941a037262daf9e26519cc98eb2282df94c6a5
buleks <>
2018-11-30 22:55:31 +0000

    I2C interface change to 24c64 eeprom

--- Firmware/targets/arm/Src/i2c_memory.cpp
+++ Firmware/targets/arm/Src/i2c_memory.cpp
@@ -5,23 +5,16 @@
 #include "i2c_memory.h"
 #include "iwdg.h"
 
-uint8_t get_i2c_address(uint16_t memory_address) {
-    uint8_t page = static_cast<uint8_t>(memory_address >> 8);
-    page &= 0b111;
-
-    uint8_t address_7b = config::memory_base_address | page;
-    return (address_7b << 1);
-}
 
 void write_block(uint16_t address, gsl::span<const uint8_t> data) {
     HAL_I2C_Mem_Write(
-        &hi2c1, get_i2c_address(address), address & 0xFF, 0x01, const_cast<uint8_t*>(data.data()), data.size(), config::memory_timeout_ms);
+        &hi2c1, 0b10100000, address, 0x02, const_cast<uint8_t*>(data.data()), data.size(), config::memory_timeout_ms);
     HAL_IWDG_Refresh(&hiwdg);
     HAL_Delay(5);
 }
 
 void read_block(uint16_t address, gsl::span<uint8_t> data) {
-    HAL_I2C_Mem_Read(&hi2c1, get_i2c_address(address), address & 0xFF, 0x01, data.data(), data.size(), config::memory_timeout_ms);
+    HAL_I2C_Mem_Read(&hi2c1,0b10100000, address & 0xFF, 0x02, data.data(), data.size(), config::memory_timeout_ms);
     HAL_IWDG_Refresh(&hiwdg);
     HAL_Delay(5);
 }
